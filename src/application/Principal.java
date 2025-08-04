4261fbfbb4e34f67e468e260ccb01905439b7dca
jeanaragaosouza <tuning.jean@hotmail.com>
2025-08-04 00:42:56 -0300

    Solução boa

--- src/application/Principal.java
+++ src/application/Principal.java
@@ -7,24 +7,24 @@ import java.util.Locale;
 import java.util.Scanner;
 
 import model.entities.Reserva;
+import model.exceptions.ExcecaoDominio;
 
 public class Principal {
 
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
-		System.out.print("Número do quarto: ");
-		int numero = sc.nextInt();
-		System.out.println("Data de check-in (dd/MM/yyyy): ");
-		Date checkin = sdf.parse(sc.next());
-		System.out.println("Data de check-out (dd/MM/yyyy): ");
-		Date checkout = sdf.parse(sc.next());
-		
-		if (!checkout.after(checkin)) {
-			System.out.println("Erro na reserva: A data de checkout deve ser maior que a data de checkin");			
-		}else {
+		try {
+			
+			System.out.print("Número do quarto: ");
+			int numero = sc.nextInt();
+			System.out.println("Data de check-in (dd/MM/yyyy): ");
+			Date checkin = sdf.parse(sc.next());
+			System.out.println("Data de check-out (dd/MM/yyyy): ");
+			Date checkout = sdf.parse(sc.next());
+			
 			Reserva reserva = new Reserva(numero, checkin, checkout);
 			System.out.println("Reserva: " + reserva);
 			
@@ -36,17 +36,17 @@ public class Principal {
 			System.out.println("Data de check-out (dd/MM/yyyy): ");	
 			checkout = sdf.parse(sc.next());
 		
-			String error = reserva.atualizaData(checkin, checkout);
-			if(error != null) {
-				System.out.println("Erro na Reserva: " + error);
-			}else {	
-				System.out.println("Reserva: " + reserva);			
-			}		
-			
+			reserva.atualizaData(checkin, checkout);
+			System.out.println("Reserva: " + reserva);			
+		} catch (ParseException e) {
+			System.out.println("Formato da data inválido!");
+		} catch (ExcecaoDominio e) {
+			System.out.println("Erro na reserva: " + e.getMessage());
+		} catch (RuntimeException e) {
+			System.out.println("Erro não esperado!");
 		}
-		
-		
-	sc.close();
+	
+		sc.close();
 	
 	}
 
--- src/model/entities/Reserva.java
+++ src/model/entities/Reserva.java
@@ -4,6 +4,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import model.exceptions.ExcecaoDominio;
+
 public class Reserva {
 	
 	private Integer numeroQuarto;
@@ -12,11 +14,13 @@ public class Reserva {
 	
 	private static SimpleDateFormat sdf =  new SimpleDateFormat("dd/MM/yyyy");
 	
-	public void Reserva() {
-		
+	public void Reserva() {		
 	}
 
-	public Reserva(Integer numeroQuarto, Date checkin, Date checkout) {		
+	public Reserva(Integer numeroQuarto, Date checkin, Date checkout) throws ExcecaoDominio {
+		if (!checkout.after(checkin)) {
+			throw new ExcecaoDominio("A data de check-out deve ser maior que a data de checkin");
+		}
 		this.numeroQuarto = numeroQuarto;
 		this.checkin = checkin;
 		this.checkout = checkout;
@@ -45,19 +49,19 @@ public class Reserva {
 		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);		
 	}
 	
-	public String atualizaData(Date checkin, Date checkout) {
+	public void atualizaData(Date checkin, Date checkout) throws ExcecaoDominio{
 		
 		Date agora = new Date();
 		
 		if(checkin.before(agora) || checkout.before(agora)) {
-			return "Erro na reserva: A data de reserva deve ser maior que a data de checkin";
+			throw new ExcecaoDominio("A data de reserva deve ser data futura");
 		}if (!checkout.after(checkin)) {
-			return "Erro na reserva: A data de check-out deve ser maior que a data de checkin";
+			throw new ExcecaoDominio("A data de check-out deve ser maior que a data de checkin");
 		}
-	
+		
 	this.checkin = checkin;
-	this.checkout = checkout;	
-	return null;	
+	this.checkout = checkout;		
+	
 	}
 	
 	@Override
